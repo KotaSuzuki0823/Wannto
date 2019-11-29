8a9c033542e32646bd95ad4f3eaa371e012b57f1
KotaSuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2019-11-29 20:14:36 +0900

    usdhioufhwoiufhw

--- main.py
+++ main.py
@@ -190,7 +190,8 @@ def testRun():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # IPアドレスとポートを指定
-        s.bind(('192.168.100.60', 50007))
+        s.bind((socket.gethostname(), 50007))
+        printOK("bind ok:{}".format(socket.gethostname()))
         # 1 接続
         s.listen(1)
         # connection するまで待つ
